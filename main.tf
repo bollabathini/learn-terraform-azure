43e47d8957928f8b3207bb7ad9852a7becca195c
Jagadeesh Bollabathini <jagadeesh.bollabathini@nemely.com>
2024-02-23 17:07:03 +0100

    replaced about referencing resource group

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ terraform {
       name = "learn-terraform-azure"
     }
   }
-
 }
 
 provider "azurerm" {
@@ -43,8 +42,8 @@ locals {
 
 resource "azurerm_key_vault" "kv" {
   name                       = "az-func-terraform"
-  location                   = var.resource_location
-  resource_group_name        = var.resource_group_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
